83e02bdabff9967c9c987920e05b4b34983f25e2
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2024-11-04 09:02:02 +0800

    文档权限 修复

--- foxconn-sw-tool-business/src/main/java/com/foxconn/sw/business/oa/SwDocumentPermissionBusiness.java
+++ foxconn-sw-tool-business/src/main/java/com/foxconn/sw/business/oa/SwDocumentPermissionBusiness.java
@@ -1,16 +1,17 @@
 package com.foxconn.sw.business.oa;
 
 import com.foxconn.sw.business.system.DepartmentBusiness;
+import com.foxconn.sw.business.system.EmployeeBusiness;
 import com.foxconn.sw.data.entity.SwDepartment;
 import com.foxconn.sw.data.entity.SwDocumentPermission;
 import com.foxconn.sw.data.entity.SwDocumentPermissionExample;
+import com.foxconn.sw.data.entity.SwEmployee;
 import com.foxconn.sw.data.mapper.extension.oa.SwDocumentPermissionExtMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class SwDocumentPermissionBusiness {
@@ -19,6 +20,8 @@ public class SwDocumentPermissionBusiness {
     SwDocumentPermissionExtMapper permissionExtMapper;
     @Autowired
     DepartmentBusiness departmentBusiness;
+    @Autowired
+    EmployeeBusiness employeeBusiness;
 
 
     public boolean insertDocumentPermission(Integer documentID, List<String> departmentIDs, int permissionType) {
@@ -34,13 +37,14 @@ public class SwDocumentPermissionBusiness {
 
     public boolean getViewPermission(String employeeNo, Integer documentID) {
         List<SwDepartment> swDepartments = departmentBusiness.getDepartment(employeeNo);
-        List<String> departmentIDs = swDepartments.stream().map(e -> e.getId().toString()).collect(Collectors.toList());
+        SwEmployee employee = employeeBusiness.selectEmployeeByENo(employeeNo);
+//        List<String> departmentIDs = swDepartments.stream().map(e -> e.getId().toString()).collect(Collectors.toList());
         SwDocumentPermissionExample example = new SwDocumentPermissionExample();
         SwDocumentPermissionExample.Criteria criteria = example.createCriteria();
         criteria.andPermissionValueEqualTo(employeeNo);
         criteria.andDocumentIdEqualTo(documentID);
         SwDocumentPermissionExample.Criteria orCriteria = example.or();
-        orCriteria.andPermissionValueIn(departmentIDs);
+        orCriteria.andPermissionValueEqualTo(employee.getDepartmentId().toString());
         orCriteria.andDocumentIdEqualTo(documentID);
         List<SwDocumentPermission> permissions = permissionExtMapper.selectByExample(example);
         return !CollectionUtils.isEmpty(permissions);
