e5ba3588209fb814769e8d427ead1713c8c544f7
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2024-12-25 13:42:07 +0800

    相同文件名判断

--- foxconn-sw-tool-service/src/main/java/com/foxconn/sw/service/processor/meeting/MeetListProcessor.java
+++ foxconn-sw-tool-service/src/main/java/com/foxconn/sw/service/processor/meeting/MeetListProcessor.java
@@ -1,38 +1,9 @@
 package com.foxconn.sw.service.processor.meeting;
 
-import com.foxconn.sw.business.meeting.MeetingBusiness;
-import com.foxconn.sw.business.meeting.MeetingCycleDetailBusiness;
-import com.foxconn.sw.business.meeting.MeetingMemberBusiness;
-import com.foxconn.sw.business.system.EmployeeBusiness;
-import com.foxconn.sw.common.context.RequestContext;
-import com.foxconn.sw.common.utils.JsonUtils;
-import com.foxconn.sw.common.utils.LocalDateExtUtils;
-import com.foxconn.sw.common.utils.StringExtUtils;
-import com.foxconn.sw.data.constants.enums.MeetingRoleFlagEnums;
-import com.foxconn.sw.data.dto.communal.CycleMeetingVo;
-import com.foxconn.sw.data.dto.entity.acount.EmployeeVo;
-import com.foxconn.sw.data.dto.entity.meeting.MeetingVo;
-import com.foxconn.sw.data.dto.request.meeting.ListMeetingParams;
-import com.foxconn.sw.data.entity.SwMeeting;
-import com.foxconn.sw.data.entity.SwMeetingCycleDetail;
-import com.foxconn.sw.data.entity.SwMeetingMember;
-import com.foxconn.sw.service.processor.MeetingRoomConfig;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
 
 @Component
 public class MeetListProcessor {
-
 
 
 }
