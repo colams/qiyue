b7d03bee0f9583c9ccea9f326500a2cbea921a1e
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-01-04 13:52:12 +0800

    专案接口修改

--- foxconn-sw-tool-business/src/main/java/com/foxconn/sw/business/project/ProjectItemBusiness.java
+++ foxconn-sw-tool-business/src/main/java/com/foxconn/sw/business/project/ProjectItemBusiness.java
@@ -1,11 +1,14 @@
 package com.foxconn.sw.business.project;
 
+import com.foxconn.sw.common.constanst.NumberConstants;
+import com.foxconn.sw.common.context.RequestContext;
 import com.foxconn.sw.data.dto.request.project.ProjectUpdateParams;
 import com.foxconn.sw.data.entity.SwProjectItem;
 import com.foxconn.sw.data.entity.SwProjectItemExample;
 import com.foxconn.sw.data.mapper.extension.project.SwProjectItemExtMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -27,17 +30,33 @@ public class ProjectItemBusiness {
 
 
     public Boolean updateProjectValue(ProjectUpdateParams params) {
-        SwProjectItem projectItem = new SwProjectItem();
-        projectItem.setProjectValue(params.getProjectValue());
-
         SwProjectItemExample example = new SwProjectItemExample();
         SwProjectItemExample.Criteria criteria = example.createCriteria();
-        criteria.andProjectIdEqualTo(params.getProjectId());
-//        criteria.andModuleTypeEqualTo(params.getModuleType());
-//        criteria.andUpdateByEqualTo(params.getUpdateBy());
-//        criteria.andDetailTypeEqualTo(params.getDetailType());
         criteria.andProjectItemEqualTo(params.getProjectItem());
+        criteria.andProjectIdEqualTo(params.getProjectId());
 
-        return projectItemExtMapper.updateByExampleSelective(projectItem, example) > 0;
+        List<SwProjectItem> items = projectItemExtMapper.selectByExample(example);
+
+        SwProjectItem newProjectItem = new SwProjectItem();
+        if (CollectionUtils.isEmpty(items)) {
+            SwProjectItemExample example1 = new SwProjectItemExample();
+            SwProjectItemExample.Criteria criteria1 = example1.createCriteria();
+            criteria1.andProjectItemEqualTo(params.getProjectItem());
+            criteria1.andProjectIdEqualTo(NumberConstants.ONE);
+            items = projectItemExtMapper.selectByExample(example1);
+
+            newProjectItem.setProjectId(params.getProjectId());
+            newProjectItem.setModuleType(items.get(0).getModuleType());
+            newProjectItem.setUpdateBy(items.get(0).getUpdateBy());
+            newProjectItem.setDetailType(items.get(0).getDetailType());
+            newProjectItem.setProjectItem(params.getProjectItem());
+            newProjectItem.setProjectValue(params.getProjectValue());
+            newProjectItem.setOperator(RequestContext.getEmployeeNo());
+        } else {
+            newProjectItem.setId(items.get(0).getId());
+            newProjectItem.setProjectValue(params.getProjectValue());
+        }
+
+        return projectItemExtMapper.insertOrUpdate(newProjectItem);
     }
 }
