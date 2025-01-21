47e3e5a24f797353d6959a0b8ee7e20dfa6363a9
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-01-21 16:56:51 +0800

    增加系统消息 接口

---
+++ foxconn-sw-tool-service/src/main/java/com/foxconn/sw/service/controller/system/NotificationController.java
@@ -0,0 +1,10 @@
+package com.foxconn.sw.service.controller.system;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/notification")
+public class NotificationController {
+        
+}
