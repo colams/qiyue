5d9c243bd5173dfe2ee48e8f0598e6f5d1d523a4
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-01-21 16:51:55 +0800

    解决冲突处理

--- foxconn-sw-tool-data/src/main/java/com/foxconn/sw/data/constants/enums/ModuleEnums.java
+++ foxconn-sw-tool-data/src/main/java/com/foxconn/sw/data/constants/enums/ModuleEnums.java
@@ -2,20 +2,18 @@ package com.foxconn.sw.data.constants.enums;
 
 public enum ModuleEnums {
 
-    Forum(1, "论坛", "forum_bbs_comment"),
-    Task(2, "工作任務", "sw_task"),
-    Notification(3, "通知消息", "sw_notification"),
+    Forum(1, "论坛,forum_bbs_comment"),
+    Task(2, "工作任務,sw_task"),
+    Notification(3, "通知消息,sw_notification"),
 
     ;
 
-    ModuleEnums(int code, String name, String tableName) {
+    ModuleEnums(int code, String tableName) {
         this.code = code;
-        this.name = name;
         this.tableName = tableName;
     }
 
     private int code;
-    private String name;
     /**
      * 关联表信息
      */
@@ -25,9 +23,6 @@ public enum ModuleEnums {
         return code;
     }
 
-    public String getName() {
-        return name;
-    }
 
     public String getTableName() {
         return tableName;
