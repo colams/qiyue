ea974c0448d7334701344fc5ef6a5cb953a6177d
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-02-11 14:43:59 +0800

    增加消息处理方法

--- foxconn-sw-tool-data/src/main/java/com/foxconn/sw/data/mapper/auto/SwMsgPoolMapper.java
+++ foxconn-sw-tool-data/src/main/java/com/foxconn/sw/data/mapper/auto/SwMsgPoolMapper.java
@@ -1,35 +1,14 @@
 package com.foxconn.sw.data.mapper.auto;
 
 import com.foxconn.sw.data.entity.SwMsgPool;
-import com.foxconn.sw.data.entity.SwMsgPoolExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.DeleteProvider;
 import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectKey;
-import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.annotations.Update;
-import org.apache.ibatis.annotations.UpdateProvider;
-import org.apache.ibatis.session.RowBounds;
-import org.apache.ibatis.type.JdbcType;
 
 @Mapper
 public interface SwMsgPoolMapper {
-    @DeleteProvider(type=SwMsgPoolSqlProvider.class, method="deleteByExample")
-    int deleteByExample(SwMsgPoolExample example);
-
-    @Delete({
-        "delete from sw_msg_pool",
-        "where id = #{id,jdbcType=INTEGER}"
-    })
-    int deleteByPrimaryKey(Integer id);
-
     @Insert({
         "insert into sw_msg_pool (msg_type, object_id, ",
         "status, create_time, ",
@@ -41,31 +20,7 @@ public interface SwMsgPoolMapper {
     @SelectKey(statement="SELECT LAST_INSERT_ID()", keyProperty="id", before=false, resultType=Integer.class)
     int insert(SwMsgPool record);
 
-    @InsertProvider(type=SwMsgPoolSqlProvider.class, method="insertSelective")
-    @SelectKey(statement="SELECT LAST_INSERT_ID()", keyProperty="id", before=false, resultType=Integer.class)
     int insertSelective(SwMsgPool record);
-
-    @SelectProvider(type=SwMsgPoolSqlProvider.class, method="selectByExample")
-    @Results({
-        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
-        @Result(column="msg_type", property="msgType", jdbcType=JdbcType.VARCHAR),
-        @Result(column="object_id", property="objectId", jdbcType=JdbcType.INTEGER),
-        @Result(column="status", property="status", jdbcType=JdbcType.INTEGER),
-        @Result(column="create_time", property="createTime", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="datetime_lastchange", property="datetimeLastchange", jdbcType=JdbcType.TIMESTAMP)
-    })
-    List<SwMsgPool> selectByExampleWithRowbounds(SwMsgPoolExample example, RowBounds rowBounds);
-
-    @SelectProvider(type=SwMsgPoolSqlProvider.class, method="selectByExample")
-    @Results({
-        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
-        @Result(column="msg_type", property="msgType", jdbcType=JdbcType.VARCHAR),
-        @Result(column="object_id", property="objectId", jdbcType=JdbcType.INTEGER),
-        @Result(column="status", property="status", jdbcType=JdbcType.INTEGER),
-        @Result(column="create_time", property="createTime", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="datetime_lastchange", property="datetimeLastchange", jdbcType=JdbcType.TIMESTAMP)
-    })
-    List<SwMsgPool> selectByExample(SwMsgPoolExample example);
 
     @Select({
         "select",
@@ -73,33 +28,6 @@ public interface SwMsgPoolMapper {
         "from sw_msg_pool",
         "where id = #{id,jdbcType=INTEGER}"
     })
-    @Results({
-        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
-        @Result(column="msg_type", property="msgType", jdbcType=JdbcType.VARCHAR),
-        @Result(column="object_id", property="objectId", jdbcType=JdbcType.INTEGER),
-        @Result(column="status", property="status", jdbcType=JdbcType.INTEGER),
-        @Result(column="create_time", property="createTime", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="datetime_lastchange", property="datetimeLastchange", jdbcType=JdbcType.TIMESTAMP)
-    })
+    @ResultMap("com.foxconn.sw.data.mapper.auto.SwMsgPoolMapper.BaseResultMap")
     SwMsgPool selectByPrimaryKey(Integer id);
-
-    @UpdateProvider(type=SwMsgPoolSqlProvider.class, method="updateByExampleSelective")
-    int updateByExampleSelective(@Param("record") SwMsgPool record, @Param("example") SwMsgPoolExample example);
-
-    @UpdateProvider(type=SwMsgPoolSqlProvider.class, method="updateByExample")
-    int updateByExample(@Param("record") SwMsgPool record, @Param("example") SwMsgPoolExample example);
-
-    @UpdateProvider(type=SwMsgPoolSqlProvider.class, method="updateByPrimaryKeySelective")
-    int updateByPrimaryKeySelective(SwMsgPool record);
-
-    @Update({
-        "update sw_msg_pool",
-        "set msg_type = #{msgType,jdbcType=VARCHAR},",
-          "object_id = #{objectId,jdbcType=INTEGER},",
-          "status = #{status,jdbcType=INTEGER},",
-          "create_time = #{createTime,jdbcType=TIMESTAMP},",
-          "datetime_lastchange = #{datetimeLastchange,jdbcType=TIMESTAMP}",
-        "where id = #{id,jdbcType=INTEGER}"
-    })
-    int updateByPrimaryKey(SwMsgPool record);
 }
