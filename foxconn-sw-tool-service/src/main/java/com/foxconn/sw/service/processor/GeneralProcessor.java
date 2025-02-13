9251ca86ca9c89ec21eb75b02fd0d557e1239d5b
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-02-13 18:05:27 +0800

    home 页面数据配置

--- foxconn-sw-tool-service/src/main/java/com/foxconn/sw/service/processor/GeneralProcessor.java
+++ foxconn-sw-tool-service/src/main/java/com/foxconn/sw/service/processor/GeneralProcessor.java
@@ -8,10 +8,10 @@ public class GeneralProcessor {
 
     public GeneralVo general() {
         GeneralVo generalVo = new GeneralVo();
-        generalVo.setScheduleTask(0);
-        generalVo.setUnreadTask(0);
-        generalVo.setCollaboration(0);
-        generalVo.setMeeting(0);
+//        generalVo.setScheduleTask(0);
+//        generalVo.setUnreadTask(0);
+//        generalVo.setCollaboration(0);
+//        generalVo.setMeeting(0);
         return generalVo;
     }
 }
