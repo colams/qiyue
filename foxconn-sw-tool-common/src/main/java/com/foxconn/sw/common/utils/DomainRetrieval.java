8ddbfb378d0ef1c55710f8e6d68670e668cd3b0f
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-02-24 16:43:08 +0800

    行程管理样式调整。暂存。

--- foxconn-sw-tool-common/src/main/java/com/foxconn/sw/common/utils/DomainRetrieval.java
+++ foxconn-sw-tool-common/src/main/java/com/foxconn/sw/common/utils/DomainRetrieval.java
@@ -6,22 +6,28 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class DomainRetrieval {
 
+    private static final String PRO_IP = "10.151.130.208";
+    private static final String PRO_Domain = "rayplusoa.efoxconn.com";
+
+
     public static String getDomain() {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
 
         String serverName = request.getServerName();
-//        int serverPort = request.getServerPort();
-//
-//        String data;
-//        if ("localhost".equals(serverName)) {
-//            String localAddr = request.getLocalAddr();
-//            data = localAddr + ":" + serverPort;
-//        } else {
-//            data = serverName + ":" + serverPort;
-//        }
+        int serverPort = request.getServerPort();
 
-        return "http://" + serverName;
+        if (PRO_Domain.equalsIgnoreCase(serverName) || PRO_IP.equalsIgnoreCase(serverName)) {
+            return String.format("https://%s", serverName);
+        }
+        String data;
+        if ("localhost".equals(serverName)) {
+            String localAddr = request.getLocalAddr();
+            data = localAddr + ":" + serverPort;
+        } else {
+            data = serverName + ":" + serverPort;
+        }
+        return String.format("http://%s", data);
     }
 
 }
