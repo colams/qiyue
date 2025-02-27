d7d77d462b23694ff5c96ac4c66e2691da62d9b4
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-02-27 18:26:32 +0800

    格式化日期信息

--- foxconn-sw-tool-service/src/main/java/com/foxconn/sw/service/processor/report/ScoreReportProcessor.java
+++ foxconn-sw-tool-service/src/main/java/com/foxconn/sw/service/processor/report/ScoreReportProcessor.java
@@ -1,7 +1,7 @@
 package com.foxconn.sw.service.processor.report;
 
-import com.foxconn.sw.data.context.RequestContext;
 import com.foxconn.sw.business.report.SwWorkReportScoreBusiness;
+import com.foxconn.sw.data.context.RequestContext;
 import com.foxconn.sw.data.dto.request.report.ScoreParams;
 import com.foxconn.sw.data.entity.SwWorkReportScore;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +17,23 @@ public class ScoreReportProcessor {
 
     public boolean score(ScoreParams data) {
         SwWorkReportScore score = scoreBusiness.queryScore(data.getEmployeeNo(), data.getYearWeek());
+
         if (Objects.nonNull(score)) {
             return scoreBusiness.updateScore(data, score);
         } else {
             SwWorkReportScore reportScore = new SwWorkReportScore();
             reportScore.setEmployeeNo(data.getEmployeeNo());
-            reportScore.setYearWeek(data.getYearWeek());
+            reportScore.setYearWeek(formatYearWeek(data.getYearWeek()));
             reportScore.setScore(data.getScore());
             reportScore.setOperator(RequestContext.getNameEmployeeNo());
             return scoreBusiness.insertScore(reportScore);
         }
+    }
+
+    private String formatYearWeek(String yearWeek) {
+        if (yearWeek.length() == 6) {
+            return yearWeek.replace("-", "-0");
+        }
+        return yearWeek;
     }
 }
