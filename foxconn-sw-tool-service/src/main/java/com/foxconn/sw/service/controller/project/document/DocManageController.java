7940e39a3cbebe2c5192ef7a3ec8b413ba89cbaa
zhangzt <zhan-teng.zhang@mail.foxconn.com>
2025-03-15 17:31:31 +0800

    帖子根据状态排序

--- foxconn-sw-tool-service/src/main/java/com/foxconn/sw/service/controller/project/document/DocManageController.java
+++ foxconn-sw-tool-service/src/main/java/com/foxconn/sw/service/controller/project/document/DocManageController.java
@@ -7,14 +7,12 @@ import com.foxconn.sw.data.dto.entity.project.doc.SearchVo;
 import com.foxconn.sw.data.dto.request.project.doc.AddFileParams;
 import com.foxconn.sw.data.dto.request.project.doc.ArchiveParams;
 import com.foxconn.sw.data.dto.request.project.doc.SearchParams;
-import com.foxconn.sw.service.aspects.Permission;
-import com.foxconn.sw.service.processor.project.doc.DocManageProcessor;
 import com.foxconn.sw.service.utils.ResponseUtils;
+import com.google.common.collect.Lists;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,64 +25,54 @@ import java.util.List;
 @RequestMapping("/api/pj/doc/docManage")
 public class DocManageController {
 
-    @Autowired
-    private DocManageProcessor docManageProcessor;
-
-    @Permission
     @Operation(summary = "搜索文件")
     @ApiResponse(responseCode = "0", description = "成功碼")
     @PostMapping("/search")
     public Response<List<SearchVo>> search(@Valid @RequestBody Request<SearchParams> request) {
-        List<SearchVo> result = docManageProcessor.search(request.getData());
+        List<SearchVo> result = Lists.newArrayList();
         return ResponseUtils.success(result, request.getTraceId());
     }
 
-    @Permission
     @Operation(summary = "文件列表")
     @ApiResponse(responseCode = "0", description = "成功碼")
     @PostMapping("/listFile")
     public Response<List<DocVo>> listFile(@Valid @RequestBody Request<Integer> request) {
-        List<DocVo> result = docManageProcessor.listFile(request.getData());
+        List<DocVo> result = Lists.newArrayList();
         return ResponseUtils.success(result, request.getTraceId());
     }
 
-    @Permission
     @Operation(summary = "添加文件")
     @ApiResponse(responseCode = "0", description = "成功碼")
     @PostMapping("/addFile")
     public Response<Boolean> addFile(@Valid @RequestBody Request<AddFileParams> request) {
-        Boolean result = docManageProcessor.addFile(request.getData());
+        Boolean result = false;
         return ResponseUtils.success(result, request.getTraceId());
     }
 
-    @Permission
     @Operation(summary = "未歸檔文件數量")
     @ApiResponse(responseCode = "0", description = "成功碼")
     @PostMapping("/countNoArchive")
     public Response<Integer> countNoArchive(@Valid @RequestBody Request<Integer> request) {
-        Integer result = docManageProcessor.countNoArchive(request.getData());
+        Integer result = 1;
         return ResponseUtils.success(result, request.getTraceId());
     }
 
-    @Permission
     @Operation(summary = "未歸檔文件")
     @ApiResponse(responseCode = "0", description = "成功碼")
     @PostMapping("/listNoArchive")
     public Response<List<DocVo>> listNoArchive(@Valid @RequestBody Request<Integer> request) {
-        List<DocVo> result = docManageProcessor.listNoArchive(request.getData());
+        List<DocVo> result = Lists.newArrayList();
         return ResponseUtils.success(result, request.getTraceId());
     }
 
-    @Permission
     @Operation(summary = "歸檔")
     @ApiResponse(responseCode = "0", description = "成功碼")
     @PostMapping("/archive")
     public Response<Boolean> archive(@Valid @RequestBody Request<ArchiveParams> request) {
-        Boolean result = docManageProcessor.archive(request.getData());
+        Boolean result = false;
         return ResponseUtils.success(result, request.getTraceId());
     }
 
-    @Permission
     @Operation(summary = "發佈")
     @ApiResponse(responseCode = "0", description = "成功碼")
     @PostMapping("/publish")
